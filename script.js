afd5c4ca87000d9994e7c6cbc91cbdfbb4395c83
Salahuddin Mahmud <76897399+s-mahmud52@users.noreply.github.com>
2022-11-02 10:59:04 +0600

    full code

---
+++ script.js
@@ -0,0 +1,16 @@
+function openCity(e, cityName){
+    let tabcontent, tablinks;
+    tabcontent = document.querySelectorAll(".tabcontent");
+    let arr = Array.from(tabcontent);
+    arr.map((item)=>{
+        item.style.display = "none";
+    });
+    tablinks = document.querySelectorAll(".tablinks");
+    let arr2 = Array.from(tablinks);
+    arr2.map((item)=>{
+        item.className = item.className.replace(" active", "");
+    });
+    document.getElementById(cityName).style.display = "block";
+    e.currentTarget.className += " active";
+
+}
